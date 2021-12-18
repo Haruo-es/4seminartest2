5d1d3ae095978395ae2558f99bc18427f0037df5
shibuya <65008051+Haruo-es@users.noreply.github.com>
2021-12-19 03:07:47 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,10 +1,39 @@
 var map;
-var marker1;
-var marker2;
+//var marker1;
+//var marker2;
 var currentInfoWindow = null;
 var markers_cafe = [];
 
 var place_cafe = [];
+
+function createData(results) {
+  for (let i=0; i<results.length; i++) {
+    x = results[i].geometry.location;
+    y = results[i].name;
+    z = results[i].price_level;
+    s = results[i].url
+    x["name"] = y;
+    x["price"] = z;
+    x["url"] = s;
+
+    switch(results[i].shop) {
+      case "cafe":
+        place_cafe.push(x);
+        break;
+    }
+  } 
+  /*
+  place:
+  {
+    i:
+      lat: xxx
+      lng: yyy
+      name: "zzz"
+      price: 0
+  }
+  */
+}
+
 
 //function initialize() {
   //var latlng = new google.maps.LatLng(33.961942,130.958662);
@@ -26,6 +55,29 @@ var place_cafe = [];
   //});
   
 //}
+//function createMarker(i, place) {
+  //var marker = new google.maps.Marker({
+    //position: { lat:place[i].lat, lng:place[i].lng },
+    //map: map,
+    //title: place[i].name,
+  });
+
+  //markers.push(marker);
+
+  //var contentStr = '<a>' + place[i].name + '<br>●Wi-Fi</a><br><a href=' + place[i].url + '>Google検索</a>';
+
+  //var infoWindow = new google.maps.InfoWindow({
+    //content: contentStr,
+  //});
+
+  //google.maps.event.addListener(marker, 'click', function(){
+    //if(currentInfoWindow != null) {
+      //currentInfoWindow.close();
+    //}
+    //infoWindow.open(map, marker);
+    //currentInfoWindow = infoWindow;
+  //});
+//}
 
 function initMap() {
   var target = document.getElementById('map');  
@@ -34,6 +86,10 @@ function initMap() {
     center: latlng,
     zoom: 16
   });
+}
+
+function createMarker(){
+  
 }
 
 function doOpen() {
