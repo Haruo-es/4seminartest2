62f67c5a42c830839b45898e6079ae30a119479f
shibuya <65008051+Haruo-es@users.noreply.github.com>
2021-12-21 14:03:48 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,9 +1,5 @@
 var map;
 var marker;
-//var marker1;
-//var marker2;
-
-//var currentInfoWindow = null;
 
 var place_cafe = [];
 var markers_cafe = [];
@@ -34,16 +30,6 @@ function initialize() {
   //marker2 = new google.maps.Marker({
     //position: m_latlng2
   //});
-//}
-
-//function initMap() {
-  //var target = document.getElementById('map');  
-  //var latlng = { lat: 35.69092, lng: 139.7002579 };
-  //map = new google.maps.Map(document.getElementById('map'), {
-    //center: latlng,
-    //zoom: 16
-  //});
-  //createData(shinjuku_data.results);
 //}
 
 function createData(results) {
